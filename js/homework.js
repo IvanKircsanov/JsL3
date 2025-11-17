0eefd8659273ac7da7247c608d313d59c58bdf42
IvanKircsanov <i.kircsanov@gmail.com>
2025-11-17 16:06:13 +0100

    17.11.25

---
+++ js/homework.js
@@ -0,0 +1,15 @@
+let result = 5 + 5 + "5";
+console.log(result, typeof result);
+
+let email = "i.kircsanov@gmail.com";
+console.log(email.includes("@"), email.length);
+
+let word1 = "My",
+  word2 = "name",
+  word3 = "is";
+let fullName = word1 + " " + word2 + " " + word3 + " Viktor";
+console.log(fullName);
+
+let userName = "Oleksandra",
+  payment = 300;
+alert(`Thank you, ${userName}! ${payment} hryvnias to be paid`);
